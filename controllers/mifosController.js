c647bda5fcd064d7d1c8e9af81bd1ce122132d46
OngidiGeofrey <ongidigeofrey@gmail.com>
2022-12-16 02:57:23 -0800

    concatenated mifos base Url

--- controllers/mifosController.js
+++ controllers/mifosController.js
@@ -1,28 +1,28 @@
 const config = require('../config.json');
-require('dotenv').config();
 
 const fetch=require('node-fetch');
+
+
 module.exports.listing = async (req, res, next) => {
 	try {
 
-	 	
-		const base64AunthenticationKey = req.headers["acces_token"];
-		fetch('https://sandbox.mkmcapitalglobal.co.ke/fineract-provider/api/v1' +'/loanproducts/?tenantIdentifier=mkmsandbox', {
-			method: 'GET',
-			headers: { 
-				'Content-Type': 'application/json',
-				'Authorization': 'Basic '+base64AunthenticationKey
-				},
-		}) .then(res => res.json()).then(response=>
-			{   
-				console.log(response);
-			   return res.status(200).json({ 'result_code': 1,'token':authorizationHeader, data: response, 'message': "Success" });
-				//console.log(response);
-			})
+		 const base64AunthenticationKey = req.headers["acces_token"];
+    fetch(`${process.env.MIFOS_URL}/loanproducts/?tenantIdentifier=mkmsandbox`, {
+        method: 'GET',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic '+base64AunthenticationKey
+            },
+    }) .then(res => res.json()).then(response=>
+        {   
+            console.log(response);
+           return res.status(200).json({ 'result_code': 1,'token':authorizationHeader, data: response, 'message': "Success" });
+            //console.log(response);
+        })
 
         
-        console.log("Ready");
-		return res.status(200).send("Ready");
+    //    console.log("Ready");
+		//return res.status(200).send("Ready");
 		 
 
 		
@@ -30,3 +30,5 @@ module.exports.listing = async (req, res, next) => {
 		return next(err);
 	}
 };
+
+
