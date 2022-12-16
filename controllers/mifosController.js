0659479cdcd0ec556389b7c9bd086b7ee7832fb4
OngidiGeofrey <ongidigeofrey@gmail.com>
2022-12-16 04:48:00 -0800

    retrieve loan product by ID

--- controllers/mifosController.js
+++ controllers/mifosController.js
@@ -1,34 +1,56 @@
-const config = require('../config.json');
+const { default: Axios } = require("axios");
 
-const fetch=require('node-fetch');
+require("dotenv").config();
 
 
+// list all loan products
 module.exports.listing = async (req, res, next) => {
 	try {
-
-		 const base64AunthenticationKey = req.headers["acces_token"];
-    fetch(`${process.env.MIFOS_URL}/loanproducts/?tenantIdentifier=mkmsandbox`, {
-        method: 'GET',
-        headers: { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic '+base64AunthenticationKey
-            },
-    }) .then(res => res.json()).then(response=>
-        {   
-            console.log(response);
-           return res.status(200).json({ 'result_code': 1,'token':authorizationHeader, data: response, 'message': "Success" });
-            //console.log(response);
-        })
-
-        
-    //    console.log("Ready");
-		//return res.status(200).send("Ready");
-		 
-
 		
+		const base64AunthenticationKey = req.headers["access_token"];
+		const url = `${process.env.MIFOS_URL}/loanproducts`;
+		await Axios({
+			method: "GET",
+			url: url,
+			headers: {
+				"Content-Type": "application/json",
+				"Fineract-Platform-TenantId": `${process.env.MIFOS_TENANT_ID}`,
+				'Authorization': 'Basic '+base64AunthenticationKey
+			},
+		}).then((response) => {
+			res.json({
+				status: "success",
+				result: response.data,
+			});
+		});
 	} catch (err) {
 		return next(err);
 	}
 };
 
-
+// retrieve  the loan product by Id
+module.exports.get_loan_product_by_id = async (req, res, next) => {
+	try {
+		
+		let product_id=req.params.id;
+		const base64AunthenticationKey = req.headers["access_token"];
+		const url = `${process.env.MIFOS_URL}/loanproducts/${product_id}`;
+		console.log(url);
+		await Axios({
+			method: "GET",
+			url: url,
+			headers: {
+				"Content-Type": "application/json",
+				"Fineract-Platform-TenantId": `${process.env.MIFOS_TENANT_ID}`,
+				'Authorization': 'Basic '+base64AunthenticationKey
+			},
+		}).then((response) => {
+			res.json({
+				status: "success",
+				result: response.data,
+			});
+		});
+	} catch (err) {
+		return next(err);
+	}
+};
--- routes/mifosRoute.js
+++ routes/mifosRoute.js
@@ -3,5 +3,8 @@ const router = Router();
 const mifosController = require('../controllers/mifosController');
 
 router.post('/loanproducts/listing',mifosController.listing);
+router.post('/loanproducts/:id',mifosController.get_loan_product_by_id);
+router.post('/loan/:client_id',mifosController.get_loan_product_by_id);
+
 
 module.exports = router;
