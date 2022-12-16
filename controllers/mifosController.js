6de1f28129053c15d0db7adc7c82314b11e8b211
Muriithi Ian <imuriithiian@gmail.com>
2022-12-16 10:22:32 +0300

    Fix: routes

--- controllers/mifosController.js
+++ controllers/mifosController.js
@@ -1,1 +1,26 @@
-require('dotenv').config();
+const { default: Axios } = require("axios");
+
+require("dotenv").config();
+
+module.exports.login = async (req, res, next) => {
+	try {
+		const username = req.body.username;
+		const password = req.body.password;
+		const url = `${process.env.MIFOS_URL}/self/authentication?username=${username}&password=${password}`;
+		await Axios({
+			method: "post",
+			url: url,
+			headers: {
+				"Content-Type": "application/json",
+				"Fineract-Platform-TenantId": `${process.env.MIFOS_TENANT_ID}`,
+			},
+		}).then((response) => {
+			res.json({
+				status: "success",
+				result: response.data,
+			});
+		});
+	} catch (err) {
+		return next(err);
+	}
+};
--- routes/mifosRoute.js
+++ routes/mifosRoute.js
@@ -1,4 +1,9 @@
 const {Router} = require('express');
 const router = Router();
+const axios = require('axios');
+const {login} = require('../controllers/mifosController');
+
+router.post('/mifos-login', login);
+// router.post('/mifos-register', mifosController.register);
 
 module.exports = router;
