8bb88e83cfbcfdf98693466c8c55e644ea531210
OngidiGeofrey <ongidigeofrey@gmail.com>
2022-12-16 02:45:29 -0800

    loan listing

--- controllers/mifosController.js
+++ controllers/mifosController.js
@@ -1,1 +1,32 @@
+const config = require('../config.json');
 require('dotenv').config();
+
+const fetch=require('node-fetch');
+module.exports.listing = async (req, res, next) => {
+	try {
+
+	 	
+		const base64AunthenticationKey = req.headers["acces_token"];
+		fetch('https://sandbox.mkmcapitalglobal.co.ke/fineract-provider/api/v1' +'/loanproducts/?tenantIdentifier=mkmsandbox', {
+			method: 'GET',
+			headers: { 
+				'Content-Type': 'application/json',
+				'Authorization': 'Basic '+base64AunthenticationKey
+				},
+		}) .then(res => res.json()).then(response=>
+			{   
+				console.log(response);
+			   return res.status(200).json({ 'result_code': 1,'token':authorizationHeader, data: response, 'message': "Success" });
+				//console.log(response);
+			})
+
+        
+        console.log("Ready");
+		return res.status(200).send("Ready");
+		 
+
+		
+	} catch (err) {
+		return next(err);
+	}
+};
--- routes/mifosRoute.js
+++ routes/mifosRoute.js
@@ -1,4 +1,7 @@
 const {Router} = require('express');
 const router = Router();
+const mifosController = require('../controllers/mifosController');
+
+router.post('/loanproducts/listing',mifosController.listing);
 
 module.exports = router;
