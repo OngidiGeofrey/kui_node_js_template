a829a4687c918a082e519fef8f0c9e2526efaf57
OngidiGeofrey <ongidigeofrey@gmail.com>
2022-12-18 04:49:03 -0800

    loans summary

--- controllers/mifosController.js
+++ controllers/mifosController.js
@@ -35,7 +35,91 @@ module.exports.get_loan_product_by_id = async (req, res, next) => {
 		let product_id=req.params.id;
 		const base64AunthenticationKey = req.headers["access_token"];
 		const url = `${process.env.MIFOS_URL}/loanproducts/${product_id}`;
-		console.log(url);
+		
+		await Axios({
+			method: "GET",
+			url: url,
+			headers: {
+				"Content-Type": "application/json",
+				"Fineract-Platform-TenantId": `${process.env.MIFOS_TENANT_ID}`,
+				'Authorization': 'Basic '+base64AunthenticationKey
+			},
+		}).then((response) => {
+			res.json({
+				status: "success",
+				result: response.data,
+			});
+		});
+	} catch (err) {
+		return next(err);
+	}
+};
+
+// list all loans
+module.exports.get_loan_applications = async (req, res, next) => {
+	try {
+		
+		
+		const base64AunthenticationKey = req.headers["access_token"];
+		const url = `${process.env.MIFOS_URL}/loans`;
+		
+		await Axios({
+			method: "GET",
+			url: url,
+			headers: {
+				"Content-Type": "application/json",
+				"Fineract-Platform-TenantId": `${process.env.MIFOS_TENANT_ID}`,
+				'Authorization': 'Basic '+base64AunthenticationKey
+			},
+		}).then((response) => {
+			res.json({
+				status: "success",
+				result: response.data,
+			});
+		});
+	} catch (err) {
+		return next(err);
+	}
+};
+
+
+
+// retrieve client loans account
+module.exports.client_accounts = async (req, res, next) => {
+	try {
+		
+		let client__id=req.params.id;
+		const base64AunthenticationKey = req.headers["access_token"];
+		const url = `${process.env.MIFOS_URL}/clients/${client__id}/accounts`;
+		
+		await Axios({
+			method: "GET",
+			url: url,
+			headers: {
+				"Content-Type": "application/json",
+				"Fineract-Platform-TenantId": `${process.env.MIFOS_TENANT_ID}`,
+				'Authorization': 'Basic '+base64AunthenticationKey
+			},
+		}).then((response) => {
+			res.json({
+				status: "success",
+				result: response.data,
+			});
+		});
+	} catch (err) {
+		return next(err);
+	}
+};
+
+
+
+// retrieve client loans summary
+module.exports.client_summary = async (req, res, next) => {
+	try {
+		
+		let R_client__id=req.params.id;
+		const base64AunthenticationKey = req.headers["access_token"];
+		const url = `${process.env.MIFOS_URL}/runreports/ClientSummary?R_clientId=${R_client__id}&genericResultSet=false`;
 		await Axios({
 			method: "GET",
 			url: url,
--- routes/mifosRoute.js
+++ routes/mifosRoute.js
@@ -1,10 +1,18 @@
 const {Router} = require('express');
 const router = Router();
 const mifosController = require('../controllers/mifosController');
-
+//loan products listing endpoint
 router.post('/loanproducts/listing',mifosController.listing);
+
+//retrieve a loan product
 router.post('/loanproducts/:id',mifosController.get_loan_product_by_id);
-router.post('/loan/:client_id',mifosController.get_loan_product_by_id);
 
+//list loans
+router.post('/loans/listing',mifosController.get_loan_applications);
 
+// list loans applied by a client
+router.post('/loans/client/:id',mifosController.client_accounts);
+
+//list client  loan summary details
+router.post('/client/summary/:id',mifosController.client_summary);
 module.exports = router;
