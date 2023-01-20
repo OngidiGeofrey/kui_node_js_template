07a0482cffb563650deb5d49396dcb55270bc3d7
OngidiGeofrey <ongidigeofrey@gmail.com>
2023-01-20 05:09:25 -0800

    added more attributes to log a loan

--- models/MifosLoan.js
+++ models/MifosLoan.js
@@ -8,17 +8,16 @@ module.exports.MifosLoanModel = (sequelize) => {
                 type: DataTypes.INTEGER,
                 primaryKey: true,
                 autoIncrement: true,
-                allowNull: false,
+                allowNull: true,
             },
             clientId: {
                 type: DataTypes.INTEGER,
-                allowNull: false,
+                allowNull: true,
             },
             loanId: {
                 type: DataTypes.INTEGER,
-                allowNull: false,
+                allowNull: true,
             },
-
             loanName: {
                 type: DataTypes.STRING,
                 allowNull: true,
@@ -41,9 +40,119 @@ module.exports.MifosLoanModel = (sequelize) => {
             amountDisbursed:{
                 type:DataTypes.FLOAT,
                 allowNull:true
-            }
+            },
+            fundId:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            loanTermFrequency:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            loanTermFrequencyType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            numberOfRepayments:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            repaymentEvery:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            repaymentFrequencyType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            interestRatePerPeriod:{
+                type:DataTypes.FLOAT,
+                allowNull:true
+            },
+            amortizationType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            isEqualAmortization:{
+                type:DataTypes.BOOLEAN,
+                allowNull:true
+            },
+            isEqualAmortization:{
+                type:DataTypes.BOOLEAN,
+                allowNull:true
+            },
+            interestType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            interestCalculationPeriodType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            allowPartialPeriodInterestCalcualtion:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            transactionProcessingStrategyId:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            repaymentFrequencyNthDayType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            repaymentFrequencyDayOfWeekType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            repaymentFrequencyDayOfWeekType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            chargeId:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            amount:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            loanType:{
+                type:DataTypes.INTEGER,
+                allowNull:true
+            },
+            expectedDisbursementDate:{
+                type:DataTypes.STRING,
+                allowNull:true
+            },
+            submittedOnDate:{
+                type:DataTypes.STRING,
+                allowNull:true
+            },
+            dateFormat:{
+                type:DataTypes.STRING,
+                allowNull:true
+            },
+            locale:{
+                type:DataTypes.STRING,
+                allowNull:true
+            },
+            
+
             
             
+
+
+
+
+
+            
+            
+
+            
+
+            
+
         },
         {
             // Other model options go here
